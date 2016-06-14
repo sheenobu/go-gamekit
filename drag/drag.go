8c38504dd92e414d5b39ea741dbf3d3b61c984f8
Sheena Artrip <sheena.artrip@gmail.com>
2016-06-14 16:08:19 -0400

    Update rxgen generated sources

--- drag/drag.go
+++ drag/drag.go
@@ -1,6 +1,6 @@
 package drag
 
-//go:generate rxgen -name RxDraggable -type Draggable .
+//go:generate rxgen -name RxDraggable -type Draggable drag.go
 
 // Draggable defines an object that can be dragged
 type Draggable interface {
